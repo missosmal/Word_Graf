f00b69837ee6d0cf8cf00d988df008ab4b4427a1
Egor <egor.noskov05@mail.ru>
2024-10-28 09:11:40 +0500

    Выполненная практическая работа на оценку удовлетворительно.Graf

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Word_Graf.Context;
+using Microsoft.Win32;
 
 namespace Word_Graf
 {
@@ -23,6 +12,21 @@ namespace Word_Graf
         public MainWindow()
         {
             InitializeComponent();
+            LoadRooms();
+        }
+
+        private void Report(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Word Files (*.docx)|*.docx";
+            sfd.ShowDialog();
+            if (sfd.FileName != "")
+                OwnerContext.Report(sfd.FileName);
+        }
+        public void LoadRooms()
+        {
+            for (int i = 1; i < 20; i++)
+                Parent.Children.Add(new Elements.Room(i));
         }
     }
 }
